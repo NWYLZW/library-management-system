13a47b31bc1e102c7f119b12c990b3b2ef108c50
yijie4188 <yijie4188@gmail.com>
2021-01-05 17:00:55 +0800

    [feat] (view) 完成登陆界面

---
+++ src/main/java/com/yijie/libraryManagementSystem/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.yijie.libraryManagementSystem.config;
+
+/**
+ * @desc    AppConfig.java
+ * @author  yijie
+ * @date    2021-01-05 16:21
+ * @note    2021-01-05 16:21 yijie Created AppConfig.java file
+ */
+public class AppConfig {
+    public static String NAME = "QQ(仿)";
+    public static String VERSION = "1.0.0.0";
+}
--- src/main/java/com/yijie/libraryManagementSystem/view/Login.java
+++ src/main/java/com/yijie/libraryManagementSystem/view/Login.java
@@ -1,5 +1,6 @@
 package com.yijie.libraryManagementSystem.view;
 
+import com.yijie.libraryManagementSystem.config.AppConfig;
 import com.yijie.libraryManagementSystem.tool.FontTool;
 
 import javax.swing.*;
@@ -20,9 +21,11 @@ public class Login {
     private JTextField userNameInput;
     private JTextField passwordInput;
     private JButton loginBtn;
-    private JPanel title;
+    private JPanel titlePanel;
     private JLabel close;
     private JLabel min;
+    private JLabel icon;
+    private JLabel title;
 
     interface MinListener {
         public void emit();
@@ -30,10 +33,13 @@ public class Login {
     public MinListener minListener = null;
 
     public void created() {
+        FontTool.setFont(icon)
+                .setText("\uE656");
         FontTool.setFont(close)
                 .setText("\uE65E");
         FontTool.setFont(min)
                 .setText("\uE6B7");
+
         min.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         close.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
@@ -55,8 +61,13 @@ public class Login {
         });
     }
 
+    public void mounted() {
+        title.setText(AppConfig.NAME + "[" + AppConfig.VERSION + "]");
+    }
+
     public Login() {
         this.created();
+        this.mounted();
     }
 
     public static void show() {
