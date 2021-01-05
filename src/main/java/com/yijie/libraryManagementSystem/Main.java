4cfcec69d8afa3dd85b4bdea185881d9b9fed92b
yijie4188 <yijie4188@gmail.com>
2021-01-05 10:06:20 +0800

    [feat] (all) 初始化项目

---
+++ src/main/java/com/yijie/libraryManagementSystem/Main.java
@@ -0,0 +1,13 @@
+package com.yijie.libraryManagementSystem;
+
+/**
+ * @desc    Main.java
+ * @author  yijie
+ * @date    2021-01-05 08:56
+ * @note    2021-01-05 08:56 yijie Created Main.java file
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
