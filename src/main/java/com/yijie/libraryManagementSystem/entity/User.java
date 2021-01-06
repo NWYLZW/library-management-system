01e5651ae256d3a7b45d563c35b63eafbc559820
iBesokuse <1628204286@qq.com>
2021-01-06 17:41:08 +0800

    [feat] (entity) 完善了用户实体和连接实体

--- src/main/java/com/yijie/libraryManagementSystem/entity/User.java
+++ src/main/java/com/yijie/libraryManagementSystem/entity/User.java
@@ -10,10 +10,28 @@ import lombok.Setter;
  * @note    2021-01-06 11:01 yijie Created User.java file
  */
 public class User {
+    //pp号
     @Getter @Setter
     private String ppNum;
+    //昵称
     @Getter @Setter
     private String nickName;
+    //密码
     @Getter @Setter
-    private String password;
+    private String passWord;
+    //默认头像ID
+    @Getter @Setter
+    private int avatarId;
+    //性别
+    @Getter @Setter
+    private String sex;
+    //诞生日
+    @Getter @Setter
+    private String birthDay;
+    //注册时间
+    @Getter @Setter
+    private String registerTime;
+
+
+
 }
