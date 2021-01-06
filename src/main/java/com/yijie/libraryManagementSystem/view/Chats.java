083f664924b35d714601ae1497d1de62e0e90ac4
yijie4188 <yijie4188@gmail.com>
2021-01-06 22:22:39 +0800

    [docs] (view)

--- src/main/java/com/yijie/libraryManagementSystem/view/Chats.java
+++ src/main/java/com/yijie/libraryManagementSystem/view/Chats.java
@@ -6,7 +6,7 @@ import javax.swing.*;
  * @desc    Chats.java
  * @author  yijie
  * @date    2021-01-06 22:10
- * @logs[0] 2021-01-06 22:10 yijie Created the Chats.java file
+ * @note    2021-01-06 22:10 yijie Created the Chats.java file
  */
 public class Chats extends AbsActivity {
     private JPanel main;
